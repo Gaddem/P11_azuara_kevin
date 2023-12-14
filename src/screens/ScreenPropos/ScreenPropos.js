d2591cc9546980e394999896a8d25ad13dae1280
lmk-kevin <kevin@lmksystem.fr>
2023-12-14 13:50:51 +0100

    Clean for soutenance

--- src/screens/ScreenPropos/ScreenPropos.js
+++ src/screens/ScreenPropos/ScreenPropos.js
@@ -3,34 +3,41 @@ import BanerTitle from "../../components/BanerTitle/BanerTitle";
 import CompDropDownList from "../../components/DropDown/CompDropDownList";
 import "./StylePropos.css";
 const ScreenPropos = () => {
+  const contentsTextuels = [
+    {
+      title: "Fiabilité",
+      contentTxt:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      contentTxt:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      contentTxt:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      contentTxt:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
   return (
     <div className="flexColumnPropos">
       <BanerTitle type="propos" />
       <div className="listing">
-        <CompDropDownList
-          title={"Fiabilité"}
-          content={
-            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-          }
-        />
-        <CompDropDownList
-          title={"Respect"}
-          content={
-            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-          }
-        />
-        <CompDropDownList
-          title={"Service"}
-          content={
-            "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-          }
-        />
-        <CompDropDownList
-          title={"Sécurité"}
-          content={
-            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-          }
-        />
+        {contentsTextuels.map((itemPropos, i) => {
+          return (
+            <CompDropDownList
+              title={itemPropos.title}
+              content={itemPropos.contentTxt}
+            />
+          );
+        })}
       </div>
     </div>
   );
