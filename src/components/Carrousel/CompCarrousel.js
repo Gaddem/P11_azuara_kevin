d5576ba7bb2f37184884beb24bcd7134c6cd5704
lmk-kevin <kevin@lmksystem.fr>
2023-12-14 14:08:45 +0100

    Clean for soutenance

--- src/components/Carrousel/CompCarrousel.js
+++ src/components/Carrousel/CompCarrousel.js
@@ -3,6 +3,8 @@ import "./StyleCarrousel.css";
 
 const CompCarrousel = ({ images }) => {
   const [dataImages, setDataImages] = useState([]);
+  const [compenentsHiddens, setCompenentsHiddens] = useState(true);
+
   const [indexImg, setIndexImg] = useState(0);
 
   const handleCarrousel = (typeHandle) => {
@@ -25,32 +27,43 @@ const CompCarrousel = ({ images }) => {
   useEffect(() => {
     if (images) {
       setDataImages(images);
+      setCompenentsHiddens(images?.length > 1);
     }
   }, [images]);
 
   return (
     <div className="containerCarrousel">
-      <img
-        className="arrow arrowLeft"
-        onClick={() => {
-          handleCarrousel("prev");
-        }}
-        src={require("../../img/icons/Arrow.png")}
-        alt="arrow_left"
-      />
+      {compenentsHiddens && (
+        <img
+          className="arrow arrowLeft"
+          onClick={() => {
+            handleCarrousel("prev");
+          }}
+          src={require("../../img/icons/Arrow.png")}
+          alt="arrow_left"
+        />
+      )}
       <img
         className="carrouselImg"
         src={dataImages?.length > 0 ? dataImages[indexImg] : ""}
         alt="image_carrousel"
       />
-      <img
-        className="arrow arrowRight"
-        onClick={() => {
-          handleCarrousel("next");
-        }}
-        src={require("../../img/icons/Arrow.png")}
-        alt="arrow_right"
-      />
+      {compenentsHiddens && (
+        <h3 className="carrouselNumerotation">
+          {" "}
+          {indexImg + 1}/{dataImages.length}
+        </h3>
+      )}
+      {compenentsHiddens && (
+        <img
+          className="arrow arrowRight"
+          onClick={() => {
+            handleCarrousel("next");
+          }}
+          src={require("../../img/icons/Arrow.png")}
+          alt="arrow_right"
+        />
+      )}
     </div>
   );
 };
